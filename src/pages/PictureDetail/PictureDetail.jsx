919f3497e9929574da62ec6eb29fc9cdac8aa1ad
pookey1104 <pookey1104@naver.com>
2025-02-25 03:48:56 -0800

    ✨ Feat: 사진 상세 구현

--- src/pages/PictureDetail/PictureDetail.jsx
+++ src/pages/PictureDetail/PictureDetail.jsx
@@ -1,5 +1,75 @@
 import * as S from './PictureDetail.style';
+import BackHeader from '@components/Common/Header/BackHeader';
+import CameraButton from '@components/Common/CameraButton/CameraButton';
+import InstagramBox from './components/InstagramBox/InstagramBox';
+import PicGuideBox from './components/PicGuideBox/PicGuideBox';
+import DescriptionBox from './components/DescriptionBox/DescriptionBox';
+import Refresh from '@assets/svg/icon-refresh.svg';
+import first from '@assets/svg/icon-1st-medal.svg';
+import second from '@assets/svg/icon-2nd-medal.svg';
+import third from '@assets/svg/icon-3rd-medal.svg';
 
 export default function PictureDetail() {
-	return <S.Container>사진 상세</S.Container>;
+	return (
+		<S.Space>
+			<BackHeader />
+			<S.Container>
+				<S.BodyContainer>
+					<S.TopContainer>
+						<InstagramBox instagramID="jennierubyjane" />
+						<S.InstaImg src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqV-EpDA9QlYzrKkI-xVr6FFolVlQaqZQQbw&s" />
+						<PicGuideBox guide="샤요 궁(Palais de Chaillot) 방향에서 남쪽" />
+					</S.TopContainer>
+					<S.TextContainer>
+						<S.TitleContainer>포토 팁</S.TitleContainer>
+						<DescriptionBox
+							description="이 사진을 보면 에펠탑이 정면으로 보이고, 넓은 광장이 있으며, 바닥이 돌로 
+            포장되어 있습니다. 이런 특징을 고려했을 때, 샤요 궁(Palais de Chaillot) 
+            방향에서 남쪽을 바라보며 촬영한 사진일 가능성이 높습니다. 최대3줄입니다ㅏㅏㅏ"
+						/>
+					</S.TextContainer>
+					<S.TextContainer style={{ marginTop: '30px' }}>
+						<S.TitleContainer>
+							포토 랭킹
+							<S.RefreshImg src={Refresh} alt="새로고침" />
+						</S.TitleContainer>
+						<S.RankingContainer>
+							<S.RankingBarContainer>
+								배고프다
+								<S.RankingBar1>23.1%</S.RankingBar1>
+							</S.RankingBarContainer>
+							<S.RankingBarContainer>
+								배고프다
+								<S.RankingBar2>
+									<S.MedalImg src={second} alt="2등" />
+									69.3%
+								</S.RankingBar2>
+							</S.RankingBarContainer>
+							<S.RankingBarContainer>
+								배고프다
+								<S.RankingBar3>
+									<S.MedalImg src={first} alt="1등" />
+									87.6%
+								</S.RankingBar3>
+							</S.RankingBarContainer>
+							<S.RankingBarContainer>
+								배고프다
+								<S.RankingBar4>
+									<S.MedalImg src={third} alt="3등" />
+									47.1%
+								</S.RankingBar4>
+							</S.RankingBarContainer>
+							<S.RankingBarContainer>
+								배고프다
+								<S.RankingBar5>12.4%</S.RankingBar5>
+							</S.RankingBarContainer>
+						</S.RankingContainer>
+					</S.TextContainer>
+				</S.BodyContainer>
+				<S.ButtonContainer>
+					<CameraButton />
+				</S.ButtonContainer>
+			</S.Container>
+		</S.Space>
+	);
 }
--- src/pages/PictureDetail/PictureDetail.style.js
+++ src/pages/PictureDetail/PictureDetail.style.js
@@ -1,6 +1,178 @@
 import styled from 'styled-components';
-// import { theme } from '@styles/theme';
+import { theme } from '@styles/theme';
+
+export const Space = styled.div`
+	min-height: 100dvh;
+	background-color: ${theme.COLOR.Gray10};
+`;
 
 export const Container = styled.div`
-	${({ theme }) => theme.FONT.SEMIBOLD(18, 108)}
+	height: calc(100dvh - 54px);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	position: relative;
+`;
+
+export const BodyContainer = styled.div`
+	width: 100%;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 35px;
+	padding: 0 39px;
+`;
+
+export const TopContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 13px;
+`;
+
+export const InstaImg = styled.img`
+	width: 179px;
+	aspect-ratio: 3 / 4;
+	height: auto;
+	object-fit: cover;
+	border-radius: 5px;
+`;
+
+export const TextContainer = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+`;
+
+export const TitleContainer = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	${({ theme }) => theme.FONT.BOLD(18, 145)};
+	color: ${theme.COLOR.PrimaryBlue1};
+`;
+
+export const RefreshImg = styled.img`
+	width: 16px;
+	height: 16px;
+	cursor: pointer;
+`;
+
+export const RankingContainer = styled.div`
+	width: calc(100%);
+	display: grid;
+	grid-template-columns: repeat(5, 1fr);
+	align-items: flex-end;
+	column-gap: 24px;
+	margin-top: 25px;
+`;
+
+export const RankingBarContainer = styled.div`
+	display: flex;
+	width: 100%;
+	flex-direction: column;
+	gap: 1px;
+	align-items: center;
+	cursor: pointer;
+
+	${({ theme }) => theme.FONT.MEDIUM(12, 145)};
+	color: ${theme.COLOR.BLACK};
+`;
+
+export const RankingBar1 = styled.div`
+	width: 100%;
+	box-sizing: border-box;
+	height: 43px;
+	border-radius: 3px;
+	background-color: ${theme.COLOR.SubBlue3};
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 2px;
+
+	${({ theme }) => theme.FONT.MEDIUM(12, 145)};
+	color: ${theme.COLOR.SubBlue2};
+`;
+
+export const RankingBar2 = styled.div`
+	width: 100%;
+	height: 107px;
+	border-radius: 3px;
+	background-color: ${theme.COLOR.SubBlue2};
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 2px;
+
+	${({ theme }) => theme.FONT.MEDIUM(12, 145)};
+	color: ${theme.COLOR.WHITE};
+`;
+
+export const RankingBar3 = styled.div`
+	width: 100%;
+	height: 149px;
+	border-radius: 3px;
+	background-color: ${theme.COLOR.SubBlue1};
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 2px;
+
+	${({ theme }) => theme.FONT.MEDIUM(12, 145)};
+	color: ${theme.COLOR.WHITE};
+`;
+
+export const RankingBar4 = styled.div`
+	width: 100%;
+	height: 68px;
+	border-radius: 3px;
+	background-color: ${theme.COLOR.SubBlue3};
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 2px;
+
+	${({ theme }) => theme.FONT.MEDIUM(12, 145)};
+	color: ${theme.COLOR.SubBlue2};
+`;
+
+export const RankingBar5 = styled.div`
+	width: 100%;
+	min-height: 17px;
+	border-radius: 3px;
+	background-color: ${theme.COLOR.SubBlue3};
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-top: 2px;
+
+	${({ theme }) => theme.FONT.MEDIUM(12, 145)};
+	color: ${theme.COLOR.SubBlue2};
+`;
+
+export const MedalImg = styled.img`
+	width: 70%;
+	aspect-ratio: 1 / 1;
+	height: auto;
+`;
+
+export const ButtonContainer = styled.div`
+	width: 100%;
+	position: fixed;
+	left: transform(50%);
+	bottom: 0;
+	z-index: 100;
+
+	@media (hover: hover) and (pointer: fine) {
+		width: 440px;
+	}
 `;
