762a82af9a7d47289245b357681b0ceabce068fd
nowjiin <hyunjin117@naver.com>
2025-02-25 05:58:16 +0000

    🔧 Chore: 로고 디자인 및 서비스명 수정

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import * as S from './Error.style';
+import ErrorLogo from '@assets/svg/logo-error.svg';
+import LogoHeader from '@components/Common/Header/LogoHeader';
 
 export default function Error() {
-	return <S.Container>404 페이지</S.Container>;
+	const navigate = useNavigate();
+
+	const handleNavLinkClick = path => {
+		navigate(path);
+	};
+
+	return (
+		<S.Space>
+			<LogoHeader />
+			<S.Container>
+				<S.ErrorImg src={ErrorLogo} alt="Error" />
+				<S.Text>죄송합니다. 페이지를 찾을 수 없습니다.</S.Text>
+				<S.ButtonContainer onClick={() => handleNavLinkClick('/map')}>홈으로 돌아가기</S.ButtonContainer>
+			</S.Container>
+		</S.Space>
+	);
 }
--- src/pages/Error/Error.style.js
+++ src/pages/Error/Error.style.js
@@ -1,6 +1,37 @@
 import styled from 'styled-components';
-// import { theme } from '@styles/theme';
+import { theme } from '@styles/theme';
+
+export const Space = styled.div`
+	height: 100dvh;
+	background-color: ${theme.COLOR.Gray10};
+`;
 
 export const Container = styled.div`
-	${({ theme }) => theme.FONT.SEMIBOLD(18, 108)}
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	box-sizing: border-box;
+	padding: 130px 0;
+	gap: 18px;
+`;
+
+export const ErrorImg = styled.img`
+	width: 177px;
+	height: auto;
+`;
+
+export const Text = styled.div`
+	${({ theme }) => theme.FONT.SEMIBOLD(18, 145)}
+	color: ${theme.COLOR.BLACK};
+`;
+
+export const ButtonContainer = styled.div`
+	margin-top: 17px;
+	padding: 10px 17px;
+	border-radius: 5px;
+	background-color: ${theme.COLOR.PrimaryBlue1};
+	cursor: pointer;
+	${({ theme }) => theme.FONT.SEMIBOLD(12, 145)}
+	color: ${theme.COLOR.Gray10};
 `;
